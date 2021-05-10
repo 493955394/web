eab496c7ee386136a1018bc8950620a1f10d4e03
493955394@qq.com <493955394@qq.com>
2021-05-10 23:51:10 +0800

    test

--- src/components/person/Todo.vue
+++ src/components/person/Todo.vue
@@ -5,121 +5,99 @@
       <el-col :span="24" class="warp-breadcrum">
         <el-breadcrumb>
           <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-          <el-breadcrumb-item>个人事务</el-breadcrumb-item>
-          <el-breadcrumb-item>待办事项</el-breadcrumb-item>
+          <el-breadcrumb-item>商品管理</el-breadcrumb-item>
         </el-breadcrumb>
       </el-col>
     </el-row>
-    <!--serach-->
-    <div class="oa_search">
-      <el-form :inline="true" :model="form" class="demo-form-inline">
-        <el-form-item label="标题">
-          <el-input v-model="form.name" placeholder="请输入标题"></el-input>
-        </el-form-item>
-        <el-form-item label="发起人">
-          <el-input v-model="form.person" placeholder="请输入发起人"></el-input>
-        </el-form-item>
-        <el-form-item label="开始时间">
-          <el-date-picker
-            v-model="form.time"
-            type="daterange"
-            range-separator="至"
-            start-placeholder="开始日期"
-            end-placeholder="结束日期">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" icon="el-icon-search" @click="onSubmit">查询</el-button>
-        </el-form-item>
-      </el-form>
-    </div>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <!--serach-->
+        <div class="oa_search">
+          <el-form :inline="true" :model="form" class="demo-form-inline">
+            <el-form-item label="商品查询">
+              <el-input v-model="form.name" placeholder="请输入查询条件"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" icon="el-icon-search" @click="onSubmit">查询</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </el-col>
+      <el-col :span="8" :offset="4" class="add-button">
+        <el-button icon="el-icon-plus">商品种类新增</el-button>
+        <el-button icon="el-icon-plus">商品新增</el-button>
+      </el-col>
+    </el-row>
     <!--list-->
-    <el-table
-      :data="tableData4"
-      stripe
-      style="width: 100%">
-      <el-table-column
-        fixed
-        prop="id"
-        label="流水号"
-        width="120">
-      </el-table-column>
-      <el-table-column
-        prop="name"
-        label="流程名称"
-        align="center"
-        width="180">
-      </el-table-column>
-      <el-table-column
-        prop="subName"
-        align="center"
-        min-width="220"
-        label="标题">
-      </el-table-column>
-      <el-table-column
-        prop="person"
-        align="center"
-        label="发起人"
-        width="120">
-      </el-table-column>
-      <el-table-column
-        prop="time"
-        align="center"
-        label="发起时间"
-        width="120">
-      </el-table-column>
-      <el-table-column
-        prop="todoName"
-        align="center"
-        label="任务名称"
-        width="120">
-      </el-table-column>
-      <el-table-column
-        prop="notime"
-        align="center"
-        label="超时时间"
-        width="120">
-      </el-table-column>
-      <el-table-column
-        fixed="right"
-        align="center"
-        label="操作"
-        width="160">
+    <el-table :data="tableData4" stripe style="width: 100%">
+      <el-table-column prop="typeId" label="商品编码"></el-table-column>
+      <el-table-column prop="name" label="商品名称" align="center"></el-table-column>
+      <el-table-column prop="typeName" align="center" label="种类名称"></el-table-column>
+      <el-table-column prop="unit" align="center" label="计量单位"></el-table-column>
+      <el-table-column prop="specification" align="center" label="规格"></el-table-column>
+      <el-table-column fixed="right" align="center" label="操作">
         <template slot-scope="scope">
-          <el-button type="text" size="small">办理</el-button>
-          <el-button type="text" size="small">查看</el-button>
-          <el-button type="text" size="small">过程</el-button>
+          <el-button type="text" size="small" @click="edit(scope.row.id)">修改</el-button>
         </template>
       </el-table-column>
     </el-table>
   </div>
+
+  <!-- <el-pagination
+    @size-change="handleSizeChange"
+    @current-change="handleCurrentChange"
+    :current-page="currentPage4"
+    :page-sizes="[10, 20, 50, 100]"
+    :page-size="100"
+    layout="total, sizes, prev, pager, next, jumper"
+    :total="300"
+  ></el-pagination> -->
 </template>
 <script>
 export default {
   data() {
     return {
+      //tableData4: [],
       tableData4: new Array(15).fill({
-        id: 10000,
-        name: '请假申请单',
-        subName: '请假申请单—20150810-张飒',
-        person: '张飒',
-        time: '2015-10-10',
-        todoName: '部门审批',
-        notime: '2015-12-10'
+        id: "10000da",
+        typeId: "2123",
+        name: "测试商品",
+        TypeName: "手机",
+        unit: "个",
+        specification: 789023,
       }),
       form: {
-        name: '',
-        person: '',
-        time: []
-      }
+        name: "",
+      },
     };
   },
+  mounted() {
+    // 初始化方法
+    this.init();
+  },
   methods: {
+    init() {
+      // todo 获取数据
+    },
     onSubmit() {
+      // 查询框
       console.log(this.form);
-    }
-  }
+    },
+    edit(id) {
+      // 编辑数据
+      console.log(id);
+    },
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
+.add-button {
+  padding-top: 20px;
+}
 </style>
